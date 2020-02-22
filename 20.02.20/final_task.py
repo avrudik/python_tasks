44a19173293465e001e94cf09670bb40a25e3e18
avrudik <avrudik@kaf65.ru>
2020-02-22 16:24:49 +0300

    made final task work

--- 20.02.20/final_task.py
+++ 20.02.20/final_task.py
@@ -15,7 +15,7 @@ for row in data:
         if row[0] == '1' and row[1] == 'female':
             survivors.append(row)
 
-for rows, lists in zip(sheet.iter_rows(max_row=len(survivors), max_col=len(survivors[0])+2), survivors):
+for rows, lists in zip(sheet.iter_rows(max_row=len(survivors), max_col=len(survivors[0])), survivors):
     for cell, data in zip(rows, lists):
         cell.value = data
 book.save('female_survivors.xlsx')
