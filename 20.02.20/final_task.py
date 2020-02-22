3f6d47a27395e6ed67b96f2c6d9f4c807d2330f6
avrudik <avrudik@kaf65.ru>
2020-02-22 16:01:27 +0300

    made final task work

--- 20.02.20/final_task.py
+++ 20.02.20/final_task.py
@@ -1,23 +1,21 @@
 import csv
 import openpyxl
 
-
 with open('titanic.csv', 'r') as f:
     reader = csv.reader(f, delimiter=',')
     data = [row for row in reader]
-    book = openpyxl.Workbook()
-    sheet = book.active
 
-    survivors = (data[0])
+book = openpyxl.Workbook()
+sheet = book.active
 
-    for rows in data:
-        if rows != data[0]:
-            survivors.append(rows)
+survivors = [data[0]]
 
-    for rows in survivors:
-        if rows[0] == '1' and rows[1] == 'female':
-            survivors.append(rows)
-            sheet.append(rows)
+for row in data:
+    if row != data[0]:
+        if row[0] == '1' and row[1] == 'female':
+            survivors.append(row)
 
-    book.save('female_survivors.xlsx')
-
+for rows, lists in zip(sheet.iter_rows(max_row=len(survivors), max_col=len(survivors[0])+2), survivors):
+    for cell, data in zip(rows, lists):
+        cell.value = data
+book.save('female_survivors.xlsx')
