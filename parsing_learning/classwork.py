9602acd189c0a279513ee0fc80e21c6df6e968c0
avrudik <58953481+avrudik@users.noreply.github.com>
2020-03-12 19:35:06 +0300

    ADDING CURRENT FILES

--- parsing_learning/classwork.py
+++ parsing_learning/classwork.py
@@ -1,31 +1,45 @@
 from selenium import webdriver
-import time
 from lxml.html import fromstring
+from time import sleep
 
 
 def get_driver():
-    agent = webdriver.Firefox(executable_path='geckodriver.exe')
+    agent = webdriver.Firefox(executable_path='geckodriver_32.exe')
 
     return agent
 
 
-def get_source_page(url, agent):
+def get_source_code(agent, url):
     agent.get(url)
 
     return fromstring(agent.page_source)
 
 
-def get_movie_title(html):
-    title = html.xpath('//*[@id="headerFilm"]/h1/span/text()')
-    return title
+def get_table_pages(agent, url):
+    source_code = get_source_code(agent, url)
+    pages = source_code.xpath('//ul[@class="pagination"]/li/a/@href')
+    return list(set(pages))
+
+
+def get_data(agent, link, pages):
+    headers = []
+    for page in pages:
+        full_link = link + page
+        source_code = get_source_code(agent, full_link)
+        headers.append(source_code.xpath('//thead/tr/th/a/text()'))
+    return headers
 
 
 if __name__ == '__main__':
     driver = get_driver()
-    source_code = get_source_page("https://www.kinopoisk.ru/film/1080513/", driver)
-    print(get_movie_title(source_code))
+    urls = [
+        'http://kaf65.mephi.ru/tutorial/datasource_first/',
+        'http://kaf65.mephi.ru/tutorial/datasource_second/',
+        'http://kaf65.mephi.ru/tutorial/datasource_third/'
+    ]
 
-    time.sleep(3)
+    for url in urls:
+        print(get_data(driver, url, get_table_pages(driver, url)))
+
+
     driver.close()
-
-# //*[@id="headerFilm"]/h1/span
